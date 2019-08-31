c9d508fc884e3fc21ec4930f840d01c33a15ff50
rolandn <roland.neyrinck@gmail.com>
2019-08-31 11:09:25 +0200

    Premier commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Roland NEYRINCK
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': "Exam_2019",
+    'version': '10.0.1',
+    'category': 'Generic Modules/Accounting',
+    'author':   "Appalach SPRL - Roland NEYRINCK",
+    'website': '',
+'description': """
+        Module custom pour le cours de M Casalicchio.
+    """,
+    'license': 'AGPL-3',
+    'depends': ['account','sale'],
+    'data': [
+
+    ],
+    'installable': True,
+}
